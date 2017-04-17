fb230ec2672802534beb5daed5d55277e7e4f18c
codeyu <codeyu@gmail.com>
2017-04-17 21:56:53 +0800

    go-leap ok

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,6 +2,11 @@ package leap
 
 const testVersion = 3
 
-func IsLeapYear(int) bool {
+func IsLeapYear(year int) bool {
 	// Write some code here to pass the test suite.
+	if mod := year % 100; mod != 0 {
+		return year%4 == 0
+	}
+	return year%400 == 0
+
 }
