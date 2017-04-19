b8c525efbfd9362f04f1e5c2201bbd85b06644d8
codeyu <codeyu@gmail.com>
2017-04-19 23:56:30 +0800

    init

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,6 +4,11 @@
 // You can document general stuff about the package here if you like.
 package clock
 
+import (
+	"fmt"
+	"math"
+)
+
 // The value of testVersion here must match `targetTestVersion` in the file
 // clock_test.go.
 const testVersion = 4
@@ -11,16 +16,57 @@ const testVersion = 4
 // Clock API as stub definitions.  No, it doesn't compile yet.
 // More details and hints are in clock_test.go.
 
-type Clock // Complete the type definition.  Pick a suitable data type.
-
-func New(hour, minute int) Clock {
+type Clock struct {
+	Hour   int
+	Minute int
 }
 
-func (Clock) String() string {
+func New(hour int, minute int) *Clock {
+	var c = Clock{0, 0}
+	c.Hour = hour
+	c.Minute = minute
+	return &c
 }
 
-func (Clock) Add(minutes int) Clock {
+func (clock Clock) String() string {
+	hour, minute := getAdjustedMinuteAndReturnAccumulatedHour(clock.Minute)
+	hour = getAdjustedHour(clock.Hour + hour)
+	return fmt.Sprintf("%02d:%02d", hour, minute)
 }
 
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
+func (clock *Clock) Add(minutes int) Clock {
+	clock.Minute += clock.Minute + minutes
+	return *clock
+}
+
+func getAdjustedHour(num int) int {
+	hour := 0
+	switch {
+	case num >= 0 && num < 24:
+		hour = num
+
+	case num >= 24:
+		hour = int(math.Mod(float64(num), 24))
+
+	case num < 0:
+		hour = 24 - int(math.Mod(math.Abs(float64(num)), 24))
+	}
+	return hour
+}
+
+func getAdjustedMinuteAndReturnAccumulatedHour(num int) (int, int) {
+	hour, minute := 0, 0
+	switch {
+	case num >= 0 && num < 60:
+		minute = num
+
+	case num >= 60:
+		hour = getAdjustedHour(num / 60)
+		minute = int(math.Mod(float64(num), 60))
+
+	case num < 0:
+		minute = 60 - int(math.Mod(math.Abs(float64(num)), 60))
+		hour = getAdjustedHour(num / 60)
+	}
+	return hour, minute
+}
