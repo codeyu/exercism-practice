44757c2ffc10d0f31b99563646f923e7c13ac7c9
codeyu <codeyu@gmail.com>
2017-04-25 22:34:02 +0800

    Bob test

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,42 @@
 using System;
-
+using System.Linq;
 public static class Bob
 {
     public static string Hey(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sure = "Sure.";
+        var yell = "Whoa, chill out!";
+        var ignore = "Fine. Be that way!";
+        var other = "Whatever.";
+        bool isYell(string str)
+        {
+            var hasUpper = false;
+            foreach(var c in str)
+            {
+                if(Char.IsLetter(c))
+                {
+                    if(Char.IsUpper(c))
+                    {
+                        hasUpper = true;
+                        continue;
+                    }
+                    return false;
+                }
+            }
+            return hasUpper;
+        }
+        if(string.IsNullOrWhiteSpace(statement))
+        {
+            return ignore;
+        }
+        else if(isYell(statement))
+        {
+            return yell;
+        }
+        else if(statement.Trim().EndsWith("?"))
+        {
+            return sure;
+        }
+        return other;
     }
 }
