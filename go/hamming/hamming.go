9d76d6a810d1a99b90855d65a70ca20742c50a06
codeyu <codeyu@gmail.com>
2017-05-01 20:53:27 +0800

    go hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,6 +1,18 @@
 package hamming
 
+import "errors"
+
 const testVersion = 5
 
 func Distance(a, b string) (int, error) {
+	if len(a)-len(b) != 0 {
+		return -1, errors.New("two DNA strands not equel")
+	}
+	dis := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			dis = dis + 1
+		}
+	}
+	return dis, nil
 }
