8cc00297f281eb849f428af17e3ac7e812ec6787
codeyu <codeyu@gmail.com>
2017-05-02 22:32:53 +0800

    go raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,8 +1,24 @@
 package raindrops
-
+import "strconv"
 const testVersion = 3
 
-func Convert(int) string
-
-// Don't forget the test program has a benchmark too.
-// How fast does your Convert convert?
+func Convert(in int) string{
+	var str = ""
+	var has_factor = false
+	if in % 3 == 0{
+		has_factor = true
+		str += "Pling"
+	}
+	if in % 5 ==0{
+		has_factor = true
+		str += "Plang"
+	}
+	if in % 7 == 0{
+		has_factor = true
+		str += "Plong"
+	}
+	if has_factor{
+		return str
+	}
+	return strconv.Itoa(in)
+}
