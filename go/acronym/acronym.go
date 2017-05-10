e943906fdf142196f6eb7a754ef99340c526f11a
codeyu <codeyu@gmail.com>
2017-05-10 23:46:06 +0800

    acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,5 +1,19 @@
 package acronym
 
+import (
+	"regexp"
+	"strings"
+)
+
 const testVersion = 2
 
-func Abbreviate(string) string
+func Abbreviate(phrase string) (acr string) {
+	acronym := []string{}
+	re := regexp.MustCompile("[A-Z]+[a-z]*|[a-z]+")
+	words := re.FindAllString(phrase, -1)
+	for _, w := range words {
+		acronym = append(acronym, w[0:1])
+	}
+	acr = strings.ToUpper(strings.Join(acronym, ""))
+	return
+}
