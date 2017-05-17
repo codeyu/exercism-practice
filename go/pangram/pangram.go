82b585c36ff25042e4d4b6861a0bfdfda49948f9
codeyu <codeyu@gmail.com>
2017-05-17 23:31:45 +0800

    not ..

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,11 @@
 package pangram
-
+import (
+	"regexp"
+	"strings"
+)
 const testVersion = 1
 
-func IsPangram(string) bool
+func IsPangram(input string) bool｛
+	reg := regexp.MustCompile("[a-zA-Z]")
+	letter := reg.FindAllString(input, -1)
+｝
