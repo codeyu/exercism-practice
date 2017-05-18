253a0cc29289defa55dfdc39beee34ac3659958b
codeyu <codeyu@gmail.com>
2017-05-19 07:29:01 +0800

    pangram ok

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,11 +1,29 @@
 package pangram
+
 import (
 	"regexp"
 	"strings"
 )
+
 const testVersion = 1
 
-func IsPangram(input string) bool｛
-	reg := regexp.MustCompile("[a-zA-Z]")
-	letter := reg.FindAllString(input, -1)
-｝
+func IsPangram(input string) bool {
+
+	reg := regexp.MustCompile("[a-z]")
+	letter := reg.FindAllString(strings.ToLower(input), -1)
+	UniqueSlice(&letter)
+	return len(letter) == 26
+}
+func UniqueSlice(slice *[]string) {
+	found := make(map[string]bool)
+	total := 0
+	for i, val := range *slice {
+		if _, ok := found[val]; !ok {
+			found[val] = true
+			(*slice)[total] = (*slice)[i]
+			total++
+		}
+	}
+
+	*slice = (*slice)[:total]
+}
