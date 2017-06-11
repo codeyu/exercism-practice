f35cd619300970a329ab6c757d1cd66581046de6
codeyu <codeyu@gmail.com>
2017-06-11 22:42:11 +0800

    go bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,7 +1,32 @@
 package bob
 
+import (
+	"strings"
+	"unicode"
+)
+
 const testVersion = 3
 
 func Hay(input string) string {
-
+	isAllUpper := false
+	for _, ch := range input {
+		if unicode.IsLetter(ch) {
+			if unicode.IsUpper(ch) {
+				isAllUpper = true
+			} else {
+				isAllUpper = false
+				break
+			}
+		}
+		continue
+	}
+	if isAllUpper {
+		return "Whoa, chill out!"
+	} else if input[len(input)-1] == '?' {
+		return "Sure."
+	} else if len(strings.TrimSpace(input)) == 0 {
+		return "Fine. Be that way!"
+	} else {
+		return "Whatever."
+	}
 }
